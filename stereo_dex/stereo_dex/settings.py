a9cea125dfa569a7d5eecc4cf6c7083d19c87c78
pedro-rampazo <pedro@pop-os.localdomain>
2023-01-27 19:30:00 -0300

    connecting mysql database

--- stereo_dex/stereo_dex/settings.py
+++ stereo_dex/stereo_dex/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'stereo_dex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
